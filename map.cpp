6b60c2351f58ee0d99ae04b6ec1e3cfd82441855
Ayush19-01 <57313104+Ayush19-01@users.noreply.github.com>
2020-01-05 16:32:26 +0530

    Update and rename maps.cpp to map.cpp

---
+++ map.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+
+using namespace std;
+
+int main()
+ {
+	map<int, int>m1;
+	
+	m1.insert(pair<int, int>(1,52));
+	m1.insert(pair<int, int>(67,87));
+	m1.insert(pair<int, int>(43,12));
+	m1.insert(pair<int, int>(9,45));
+	m1.insert(pair<int, int>(69, 59));
+	
+	map<int, int>::iterator itr;
+	cout << "\nThe map is : \n";
+	cout << "KEY    Value\n";
+	for (itr =m1.begin(); itr !=m1.end(); ++itr) {
+		cout << itr->first
+		     << "      " << itr->second << '\n';
+	}
+	
+	cout<<"\nThe size of the map is : "<< m1.size();
+
+}
